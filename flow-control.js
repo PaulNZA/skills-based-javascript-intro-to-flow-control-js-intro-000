f1a06222e01449dbcf518d96600b6d767a975c81
Paul Nieuwoudt <paulnieuwoudt@gmail.com>
2020-03-31 17:07:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,31 @@
-function basicTeenager(age) {
 
+
+function basicTeenager(age) {
+  if(age>=13 && age<=19) {return "You are a teenager!"
+}
 }
 
-function teenager(age) {
 
+
+
+function teenager(age) {
+  if(age>=13 && age<=19) {return "You are a teenager!"
+}
+else {
+  return "You are not a teenager"
+}
 }
 
 function ageChecker(age) {
+  if (age <= 12) {return "You are a kid"
+}
+else if {
+  (age >19) {return "You are an adult"
+}
+else {return "You are a teenager"
 
+}
+}
 }
 
 function ternaryTeenager(age) {
